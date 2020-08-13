095587667d9303e9d5f59e16b06f9239e064b688
Jason M Duncan <45959171+jduncan1980@users.noreply.github.com>
2020-08-12 22:05:42 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
           target="_blank"
           rel="noreferrer noopener"
         >
-          This project
+          This projekt
         </a>{' '}
         was bootstrapped with{' '}
         <a href="https://facebook.github.io/create-react-app/">
